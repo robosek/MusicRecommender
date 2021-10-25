775f7effcc74908a6e323a0e099fa6d6c09af6a5
robosek <robosek@tlen.pl>
2021-10-25 23:37:11 +0200

    Init commit

---
+++ MusicRecommender/Recommendation/Adapter/In/FindRecommendationsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicRecommender.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FindRecommendationsController : ControllerBase
+    {
+        private static readonly string[] Recommendations = new[]
+        {
+             "Cool", "Hot"
+        };
+
+        public FindRecommendationsController()
+        { }
+
+        [HttpGet]
+        public IEnumerable<string> Get() => Recommendations;
+    }
+}
