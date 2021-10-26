b8529ec5eaa29a85961c958c9374939fe92b8991
robosek <robosek@tlen.pl>
2021-10-26 10:52:06 +0200

    Fix Recommendation properties types. Add private constructor

--- MusicRecommender/Recommendation/Domain/Recommendation.cs
+++ MusicRecommender/Recommendation/Domain/Recommendation.cs
@@ -7,13 +7,39 @@ namespace MusicRecommender.Recommendation.Domain
 {
     public class Recommendation
     {
-        public int Author { get; }
-
-        public int SongName { get; }
-
+        public string Author { get; }
+        public string SongName { get; }
         public int Popularity { get; }
+        public string Genre { get; }
+        public int Year { get; set; }
         public Priority Priority { get; }
         
+        private Recommendation(string author, string songName, string genre, int year, int popluarity, Priority priority)
+        {
+            Author = author;
+            SongName = songName;
+            Genre = genre;
+            Year = year;
+            Popularity = popluarity;
+            Priority = priority;
+        }
+
+        //public static Recommendation Create(AuthorSearchResult authorSearchResult)
+        //{
+        //    return new Recommendation();
+        //}
+        //public static Recommendation Create(SongNameSearchResult authorSearchResult)
+        //{
+        //    return new Recommendation();
+        //}
+        //public static Recommendation Create(GenreSearchResult authorSearchResult)
+        //{
+        //    return new Recommendation();
+        //}
+        //public static Recommendation Create(YearSearchResult authorSearchResult)
+        //{
+        //    return new Recommendation();
+        //}
     }
 
     public enum Priority
